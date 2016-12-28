d822784274b1fde98159a8091a1550f111e4c0ce
francisco <francisco.disalvo90@gmail.com>
2016-12-28 18:31:45 -0300

    Commit inicial

---
+++ src/main/java/com/onready/localdate/LocaldateApplication.java
@@ -0,0 +1,12 @@
+package com.onready.localdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaldateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaldateApplication.class, args);
+	}
+}
