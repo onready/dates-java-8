a80637cb93536d1c77c24578cab61ff4db657a92
francisco <francisco.disalvo90@gmail.com>
2016-12-29 13:18:41 -0300

    Creamos la clase de dominio y la api para devolverlo

--- src/main/java/com/onready/dates/LocaldateApplication.java
+++ src/main/java/com/onready/dates/LocaldateApplication.java
@@ -1,4 +1,4 @@
-package com.onready.localdate;
+package com.onready.dates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
